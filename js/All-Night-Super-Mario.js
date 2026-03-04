a1b4e2c8fc59cf4196b6d3650fa2a1b316d43581
KIOLuo24 <158626318+KIOLuo24@users.noreply.github.com>
2026-03-04 22:37:59 +0800

    Update All-Night-Super-Mario.js

--- js/All-Night-Super-Mario.js
+++ js/All-Night-Super-Mario.js
@@ -399,7 +399,7 @@ class FCSuperMarioEmulator {
 
                 if (pageWidth > pageHeight * 2) {
                     // 宽度大于高度的 2 倍
-                    const newHeight = containerWidth / 2;
+                    const newHeight = containerWidth / 2.5;
                     canvas.style.height = newHeight + 'px';
                 } else {
                     // 宽度小于等于高度的 2 倍
@@ -580,6 +580,7 @@ function resetGame() {
 async function autoLoadLocalGame() {
     console.log('autoLoadLocalGame 已弃用，使用新的初始化方式');
 }
+
 
 
 
