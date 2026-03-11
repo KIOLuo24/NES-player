6f928ef63ed728c5fb5bb8fc5250371ce2cb12f6
KIOLuo24 <158626318+KIOLuo24@users.noreply.github.com>
2026-03-11 20:20:02 +0800

    Update Pokemon_Ruby.js

--- js/Pokemon_Ruby.js
+++ js/Pokemon_Ruby.js
@@ -6,7 +6,7 @@ class FCSuperMarioEmulator {
     constructor() {
         this.ejs = null;
         this.gameContainer = document.getElementById('gameContainer');
-        this.targetROM = 'game/GBA/Pokemon_Ruby/Pokemon_Ruby';
+        this.targetROM = 'game/GBA/Pokemon_Ruby/Pokemon_Ruby.gba';
         this.isRunning = false;
         this.isInitialized = false;
         this.audioContext = null;
@@ -580,3 +580,4 @@ function resetGame() {
 async function autoLoadLocalGame() {
     console.log('autoLoadLocalGame 已弃用，使用新的初始化方式');
 }
+
